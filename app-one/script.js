56baff3c08a851182a1a2398a7314afa9c7668f9
Sran Manpreet <manna1661@gmail.com>
2023-03-16 07:08:05 +0530

    App works

--- app-one/script.js
+++ app-one/script.js
@@ -3,11 +3,20 @@ let babyCartoon = document.getElementById("babyCartoon");
 
 jumpButton.addEventListener('click', jump);
 
-function jump(){
+function jump() {
     babyCartoon.classList.add('jump-baby');
     jumpButton.disabled = true;
-    setTimeout(()=> {
+    setTimeout(() => {
         babyCartoon.classList.remove('jump-baby');
         jumpButton.disabled = false;
     }, 2000);
+}
+
+document.body.onkeyup = function (e) {
+    if (e.key == " " ||
+        e.code == "Space" ||
+        e.keyCode == 32
+    ) {
+        jump();
+    }
 }
